0d3f4e251f5c27554f4a67653acb78dcfcf4d339
Mr777Nick <bagas_naufal96@yahoo.co.id>
2020-06-15 12:59:48 +0700

    Completing Post Reducer

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { createStore } from 'redux';
+import postReducer from './reducers/postReducer';
 
-const store = createStore();
+const store = createStore(postReducer);
 
 ReactDOM.render(
   <React.StrictMode>
