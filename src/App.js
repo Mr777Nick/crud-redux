d458723637ab2f4a7ca17b3f16665667a537912e
Mr777Nick <bagas_naufal96@yahoo.co.id>
2020-06-15 12:46:08 +0700

    Basic UI - Post & All Post

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-
+import PostForm from './components/PostForm';
+import AllPost from './components/AllPost';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React!</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <PostForm />
+                <AllPost />
+            </div>
+        );
+    }
 }
 export default App;
