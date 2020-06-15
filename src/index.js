e6dfdc8e5ccafdb2308232825ebc23165ed98e16
Mr777Nick <bagas_naufal96@yahoo.co.id>
2020-06-15 13:12:52 +0700

    Connect Post Form

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import postReducer from './reducers/postReducer';
+
 
 const store = createStore(postReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
