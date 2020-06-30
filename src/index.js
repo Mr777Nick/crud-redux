dc77ca8cd6b038e24749a690189ab241545e0b31
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2020-06-30 11:21:37 +0700

    Remove Old Store

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { postsReducer } from './reducers/postsReducer';
 import 'fontsource-roboto';
 
 
-const store = createStore(postsReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
